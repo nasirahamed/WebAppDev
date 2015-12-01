05b0abac105cee7f11e85201c8e9b0aeca9e5f3b
Nasir Ahamed <m.nasir.ahamed@gmail.com>
2015-12-01 04:51:34 +0000

    AJAX Search implemented, searches from the exisitng XML file and display the results

--- search.php
+++ search.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -15,17 +16,96 @@
     <!-- Custom CSS -->
     <link href="css/custom.css" rel="stylesheet">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
     <script src="js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
+
+    <!-- Ajax Search starts-->
+
+    <!-- Ajax Search-->
+
+    <link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css" />
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.js"></script>
+    <script type="text/javascript">
+        $.ajax({
+            url: "xml/furnitures.xml", //your url
+            type: "POST", //may not need this-fiddle did-default is GET
+            dataType: "xml",
+            //should not need to define 'data' on your own xml call
+            //This code was extracted from http://jsfiddle.net/jensbits/PekQZ/light/ then altered
+            success: function(xmlResponse) {
+                var data = $("furniture", xmlResponse).map(function() {
+                    return {
+                        value: $("name", this).text() + ", " + ($.trim($("type", this).text()) || "(unknown type)"),
+                        id: $("id", this).text()
+                    };
+                }).get();
+                $("#test").autocomplete({
+                    source: function(req, response) {
+                        var re = $.ui.autocomplete.escapeRegex(req.term);
+                        var matcher = new RegExp("^" + re, "i");
+                        response($.grep(data, function(item) {
+                            return matcher.test(item.value);
+                        }));
+                    },
+                    minLength: 2,
+                    select: function(event, ui) {
+                        $("p#result").html(ui.item ? "Selected: " + ui.item.value + ", id: " + ui.item.id : "Nothing selected, input was " + this.value);
+                    }
+                });
+            }
+        });
+    </script>
+    <style type="text/css">
+        .red {
+            color: red
+        }
+    </style>
+    <style type="text/css">
+        #custom-search-input {
+            margin: 0;
+            margin-top: 10px;
+            padding: 0;
+        }
+        
+        #custom-search-input .search-query {
+            padding-right: 3px;
+            padding-right: 4px \9;
+            padding-left: 3px;
+            padding-left: 4px \9;
+            /* IE7-8 doesn't have border-radius, so don't indent the padding */
+            margin-bottom: 0;
+            -webkit-border-radius: 3px;
+            -moz-border-radius: 3px;
+            border-radius: 3px;
+        }
+        
+        #custom-search-input button {
+            border: 0;
+            background: none;
+            /** belows styles are working good */
+            padding: 2px 5px;
+            margin-top: 2px;
+            position: relative;
+            left: -28px;
+            /* IE7-8 doesn't have border-radius, so don't indent the padding */
+            margin-bottom: 0;
+            -webkit-border-radius: 3px;
+            -moz-border-radius: 3px;
+            border-radius: 3px;
+            color: #D9230F;
+        }
+        
+        .search-query:focus + button {
+            z-index: 3;
+        }
+    </style>
+
+
 </head>
+
 <body>
     <!-- Navigation -->
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -59,7 +139,7 @@
         <div class="row">
 
             <div class="col-md-3">
-                
+
                 <div class="list-group">
                     <a href="add.php" class="list-group-item">Add Furniture</a>
                     <a href="remove.php" class="list-group-item">Remove Furniture by ID</a>
@@ -68,67 +148,52 @@
                 </div>
             </div>
 
-            <div class="col-md-9">
-
-            <form class="form-horizontal" role="form" method="POST" action="search.php">
-            <p class="lead">Search Furniture</p>
-
-            <div class="form-group">
-                    <label class="col-sm-2 control-label">Search by ID#</label>
-                        <div class="col-sm-6">
-                            <input class="form-control" type="text" name="id">
-                        </div>
+            <p class="lead">Search Furniture by Name</p>
+            <div id="custom-search-input">
+                <div class="input-group col-md-6">
+                    <input type="text" class="  search-query form-control" placeholder="Search" id="test" />
+                    <span class="input-group-btn">
+                        <button class="btn btn-danger" type="button">
+                            <span class=" glyphicon glyphicon-search"></span>
+                    </button>
+                    </span>
+                </div>
             </div>
+            <p id="result" class="ui-widget red"></p>
 
-            <div class="form-group">
-                    <label class="col-sm-2 control-label"></label>
-                        <div class="col-sm-10">
-                            <button type = "submit" class="btn btn-primary" name="insert">Search</button>
-                        </div>
-            </div>            
 
-            </form>
+            <div class="container">
                 <div class="row">
 
-              
-            
+                    <div class="col-md-9">
+                        <div class="row">
 
-                  
+                        </div>
 
-                  
-
-                    
-
-                    
+                    </div>
 
                 </div>
 
             </div>
+            <!-- /.container -->
 
-        </div>
+            <div class="container">
 
-    </div>
-    <!-- /.container -->
+                <hr>
 
-    <div class="container">
+                <!-- Footer -->
+                <footer>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <p>Copyright &copy; Inventory Management System 2016</p>
+                        </div>
+                    </div>
+                </footer>
 
-        <hr>
-
-        <!-- Footer -->
-        <footer>
-            <div class="row">
-                <div class="col-lg-12">
-                    <p>Copyright &copy; Inventory Management System 2016</p>
-                </div>
             </div>
-        </footer>
+            <!-- /.container -->
 
-    </div>
-    <!-- /.container -->
-
-    <!-- jQuery -->
-
-
+            <!-- jQuery -->
 </body>
 
 </html>
