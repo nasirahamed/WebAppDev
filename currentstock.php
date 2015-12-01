3f3da3e812f61d0bfe7646dee9808a41b99b2fa9
Nasir Ahamed <m.nasir.ahamed@gmail.com>
2015-12-01 23:00:07 +0000

    table header in the currentstock.php price heading right aligned

--- currentstock.php
+++ currentstock.php
@@ -20,12 +20,6 @@ $furnitures = $arr->furniture;
     <!-- Custom CSS -->
     <link href="css/custom.css" rel="stylesheet">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
     <script src="js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
@@ -82,7 +76,7 @@ $furnitures = $arr->furniture;
                                 <th>Name</th>
                                 <th>Type</th>
                                 <th>Color</th>
-                                <th>Price</th>
+                                <th style="text-align: right;">Price</th>
                             </tr>
                         </thead>
                         <tbody>
