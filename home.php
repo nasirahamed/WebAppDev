851ccd972872ab983086dc27301288f68201d75e
Nasir Ahamed <m.nasir.ahamed@gmail.com>
2015-12-05 18:43:59 +0000

    Added RSS link in the home.ph file

--- home.php
+++ home.php
@@ -73,7 +73,7 @@
                     <a href="remove.php" class="list-group-item">Remove Furniture by ID</a>
                     <a href="search.php" class="list-group-item">Search Furniture</a>
                     <a href="currentstock.php" class="list-group-item">Current Stock</a>
-                    <a href="rss/rss.xml" class="list-group-item" target="_blank">IMS RSS <img src="img/rss.gif" width="36" height="14"></a>
+                    <a href="rss/rss.xml" class="list-group-item" target="_blank">IMS <img src="img/rss.gif" width="36" height="14"></a>
                 </div>
                 <div>
                 	<p><strong>Welcome</strong> <?php echo $_SESSION['name']; ?></p>
@@ -111,7 +111,7 @@
                 </div>
                 <div class="row">
                     <div class="col-md-9">
-                        <p>Apps created by Nasir!</p>
+                        <!--<p>Apps created by Nasir!</p> -->
                     </div>
                 </div>
             </div>
