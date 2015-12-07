baa2a883ba69aa9b0122c264c6031baffa3192c1
Nasir Ahamed <m.nasir.ahamed@gmail.com>
2015-12-07 17:34:38 +0000

    logo added in the index.php file

--- index.php
+++ index.php
@@ -34,12 +34,6 @@ require_once 'config.php';
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/font-awesome.min.css" rel="stylesheet">
     <link href="css/login.css" rel="stylesheet">
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
    
     <!-- Include all compiled plugins (below), or include individual files as needed -->
 	<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js'></script>
@@ -53,7 +47,8 @@ require_once 'config.php';
 			<?php require_once 'templates/message.php';?>
 
 			<div class="form-header">
-				<i class="fa fa-user"></i>
+				<img src="img/ims_logo.jpg" />
+				<!-- <i class="fa fa-user"></i> -->
 			</div>
 			<form id="login-form" method="post" class="form-signin" role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 				<input name="email" id="email" type="email" class="form-control" placeholder="Email address" autofocus> 
