e40395fc05a62ad4d56519433e5f11f7577307ba
Nasir Ahamed <m.nasir.ahamed@gmail.com>
2015-12-08 09:13:56 +0000

    rss_embedded.php file added in order to show the RSS content within the page

---
+++ rss_embedded.php
@@ -0,0 +1,122 @@
+<?php require_once 'templates/header.php';?>
+<?php //loading the xml file content
+$get = file_get_contents('xml/furnitures.xml');
+$arr = simplexml_load_string($get);
+$furnitures = $arr->furniture;
+?>
+<!DOCTYPE html>
+<html lang="en">
+        <head>
+            <title>Current Stock</title>
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <meta name="description" content="IMS, Inventory Management System">
+        	<meta name="author" content="inventory, inventory, furniture, IMS">
+        	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"> <!-- Latest compiled and minified CSS -->
+        	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"> <!-- Optional theme -->
+        	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css"> <!-- Latest jQuery Smoothness library -->
+            <link href="css/custom.css" rel="stylesheet"> <!-- Custom CSS -->
+            <link href="css/bootstrap.min.css" rel="stylesheet">
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script><!-- Latest compiled and minified JavaScript -->
+        	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script><!-- Latest jQuery library -->
+        	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script> <!-- Latest jQuery UI library -->
+            <script src="js/jquery.js"></script>
+            <script src="js/bootstrap.min.js"></script>
+        </head>
+<body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">Inventory Management System</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                     <li>
+                        <a href="account.php">My Account</a>
+                    </li>
+                    
+                    <li>
+                        <a href="changepassword.php">Change Password</a>
+                    </li>
+                    
+                    <li>
+                        <a href="message.php">Send Message</a>
+                    </li>
+                    
+                    <li>
+                        <a href="logout.php">Sign out</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-3">
+                
+                <div class="list-group">
+                    <a href="add.php" class="list-group-item">Add Furniture</a>
+                    <a href="remove.php" class="list-group-item">Remove Furniture by ID</a>
+                    <a href="search.php" class="list-group-item">Search Furniture</a>
+                    <a href="currentstock.php" class="list-group-item">Current Stock</a>
+                    <a href="rss_embedded.php" class="list-group-item">RSS Embedded</a>
+                    <a href="rss/rss.xml" class="list-group-item" target="_blank"><img src="img/rss.gif" width="36" height="14"></a>
+                </div>
+            </div>
+
+            <div class="col-md-9">
+                <p class="lead">RSS Embedded</p>
+                    
+                <?php
+                    // Load XML file
+                    $xml = new DOMDocument;
+                    $xml->load('rss/rss.xml');
+                    
+                    // Load XSL file
+                    $xsl = new DOMDocument;
+                    $xsl->load('rss/rss.xsl');
+                    
+                    // Configure the transformer
+                    $proc = new XSLTProcessor;
+                    
+                    // Attach the xsl rules
+                    $proc->importStyleSheet($xsl);
+                    
+                    echo $proc->transformToXML($xml);
+                    ?>                    
+            </div>
+
+        </div>
+
+    </div>
+    <!-- /.container -->
+<div class="container"> <!-- start footer container -->
+	<hr>
+<footer> <!-- Footer Started -->
+	<div class="row">
+		<div class="col-lg-12">
+		    <p><img src="img/ims_logo.jpg"></p>
+			<p>Copyright &copy; Inventory Management System 2016</p>
+			<p>designed by <strong>Nasir</strong> & <strong>Nuth</strong></p>
+		</div>
+	</div>
+</footer>
+</div> <!-- end of footer container -->
+</body>
+</html>
